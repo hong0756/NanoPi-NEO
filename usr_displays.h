03de94b1f3d3849bd090ecc96686fcc3ad3c005c
hong0756 <57704744+hong0756@users.noreply.github.com>
2023-06-03 22:28:07 +0800

    Update usr_displays.h

--- usr_displays.h
+++ usr_displays.h
@@ -25,14 +25,14 @@ std::vector<DisplayIF*>	GetUsrDisplays()
 //	iDisplays.push_back( new Display_ILI9341_spi_TM24(270,67) );
 //	iDisplays.push_back( new Display_ILI9341_spi_TM22(270) );
 //	iDisplays.push_back( new Display_ILI9341_spi_TM22(0) );
-//	iDisplays.push_back( new Display_ILI9328_spi_TM22(270));
+	iDisplays.push_back( new Display_ILI9328_spi_TM22(270));
 //	iDisplays.push_back( new Display_ILI9225_spi(270));//,198) );
 //	iDisplays.push_back( new Display_ST7735_spi(90,3) );
 //	iDisplays.push_back( new Display_ST7789_spi(270) );
 //	iDisplays.push_back( new Display_WaveShare35_spi(0) );
 //	iDisplays.push_back( new Display_ILI9486_spi(270) );
 //	iDisplays.push_back( new Display_fbdev("/dev/fb1") );
-	iDisplays.push_back( new Display_ST7789_IPS_240x240_spi(0) );
+//	iDisplays.push_back( new Display_ST7789_IPS_240x240_spi(0) );
 //	iDisplays.push_back( new Display_ST7789_IPS_spi(270) );
 //	iDisplays.push_back( new Display_ST7735_IPS_80x160_spi(270) );
 //	iDisplays.push_back( new Display_ST7789_IPS_135x240_spi(270) );
